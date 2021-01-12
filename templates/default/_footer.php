85ff7e31077338544fa443acc410befba59afb89
abitmarty <matijn.totte@gmail.com>
2021-01-12 13:40:22 +0100

    Clean shopsec code

---
+++ templates/default/_footer.php
@@ -0,0 +1,19 @@
+<?php
+if( !defined( 'CUSTOMER_PAGE' ) )
+  exit;
+?>
+        <div id="options"><div class="print"><a href="javascript:window.print();"><?php echo $lang['print']; ?></a></div><div class="back"><a href="javascript:history.back();">&laquo; <?php echo $lang['back']; ?></a></div></div>
+      </div>
+    </div>
+  </div>
+
+  <div id="foot"><?php // footer starts here ?>
+    <div class="container">
+      <div id="copy"><?php echo $config['foot_info']; ?></div>
+
+
+    </div>
+  </div>
+</div>
+</body>
+</html>
