fda1b110eb706b671b55b564f661a2dd50e18adb
abitmarty <matijn.totte@gmail.com>
2021-01-13 15:37:39 +0100

    Try

--- templates/admin/_footer.php
+++ templates/admin/_footer.php
@@ -3,7 +3,7 @@ if( !defined( 'ADMIN_PAGE' ) )
   exit;
 ?>
         <div id="foot">
-          <div class="back">
+          <div class="back try">
             <a href="javascript:history.back();">&laquo; <?php echo $GLOBALS['lang']['back']; ?></a> <a href="./?sLang=<?php echo LANGUAGE; ?>" target="_blank"><?php echo $lang['homepage']; ?></a>
           </div>
         </div>
