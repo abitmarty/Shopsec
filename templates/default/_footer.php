deb21d7d730ca194ebcdcfeb95bc0a2ed9c28733
LVergnesUU <l.vergnes@students.uu.nl>
2021-01-14 16:58:01 +0100

    pure footer test

--- templates/default/_footer.php
+++ templates/default/_footer.php
@@ -10,7 +10,13 @@ if( !defined( 'CUSTOMER_PAGE' ) )
   <div id="foot"><?php // footer starts here ?>
     <div class="container">
       <div id="copy"><?php echo $config['foot_info']; ?></div>
-
+        <br>
+      <div class="pure-g">
+        <div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1">Algemene voorwaarden</div></div><!--
+      --><div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1">Privacy</div></div><!--
+      --><div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1">Cookies</div></div><!--
+      --><div class="pure-u-1 pure-u-lg-1-4 inline"><div class="footer_row1">Algemene voorwaarden</div></div>
+      </div>
 
     </div>
   </div>
