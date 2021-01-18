f9d21ba040e415e3f46e354f8a90316c3918b727
LVergnesUU <l.vergnes@students.uu.nl>
2021-01-18 22:34:37 +0100

    footer gere-alignt

--- templates/default/_footer.php
+++ templates/default/_footer.php
@@ -19,15 +19,14 @@ if( !defined( 'CUSTOMER_PAGE' ) )
         --><div class="pure-u-1 pure-u-md-1-3 inline">
         <div class="trust-logos--logo trust-logos--ideal"></div>
         <div class="trust-logos--logo trust-logos--mastercard"></div>
-        <div class="trust-logos--logo trust-logos--visa"></div>
-      <!--  <div class="trust-logos--logo trust-logos--applepay"></div> -->
-        </div></div>
+        <div class="trust-logos--logo trust-logos--visa"></div><!--
+    --></div><!--
 
-
-        <div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1"><a class="footer_link" href="">Algemene voorwaarden</a></div></div><!--
-      --><div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1"><a class="footer_link" href="">Privacy</a></div></div><!--
-      --><div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1"><a class="footer_link" href="">Cookies</a></div></div><!--
-      --><div class="pure-u-1 pure-u-lg-1-4 inline"><div class="footer_row1"><div><?php echo $config['foot_info']; ?></div></div></div>
+    --><div class="pure-u-1 pure-u-md-1-3 inline"><!--
+          --><div class="footer_row2"><a class="footer_link" href="">Algemene voorwaarden</a></div><!--
+      --><div class="footer_row2"><a class="footer_link" href="">Privacy</a></div><!--
+      --><div class="footer_row2"><a class="footer_link" href="">Cookies</a></div></div><!--
+      --><div class="pure-u-1 pure-u-lg-1-3 inline"><div class="footer_row2"><div><?php echo $config['foot_info']; ?></div></div>
       </div>
 
 
