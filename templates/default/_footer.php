01515e2cfcaa90dbda054d41051a14434079d30b
LVergnesUU <l.vergnes@students.uu.nl>
2021-01-18 17:34:06 +0100

    1e versie footer

--- templates/default/_footer.php
+++ templates/default/_footer.php
@@ -9,14 +9,27 @@ if( !defined( 'CUSTOMER_PAGE' ) )
 
   <div id="foot"><?php // footer starts here ?>
     <div class="container">
-      <div id="copy"><?php echo $config['foot_info']; ?></div>
+
         <br>
       <div class="pure-g">
+
+        <div class="pure-u-1 pure-u-md-1-1 inline"><div class="pb_footer_title">Plantbox</div></div>
+
+        <div class="pure-u-1 pure-u-md-1-2 inline"><div class="footer_row1">Klanten beoordelen ons met een 9.5/10</div></div><!--
+        --><div class="pure-u-1 pure-u-md-1-2 inline">
+        <div class="trust-logos--logo trust-logos--ideal"></div>
+        <div class="trust-logos--logo trust-logos--mastercard"></div>
+        <div class="trust-logos--logo trust-logos--visa"></div>
+        <div class="trust-logos--logo trust-logos--applepay"></div>
+        </div></div>
+
+
         <div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1">Algemene voorwaarden</div></div><!--
       --><div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1">Privacy</div></div><!--
       --><div class="pure-u-1 pure-u-md-1-4 inline"><div class="footer_row1">Cookies</div></div><!--
-      --><div class="pure-u-1 pure-u-lg-1-4 inline"><div class="footer_row1">Algemene voorwaarden</div></div>
+      --><div class="pure-u-1 pure-u-lg-1-4 inline"><div class="footer_row1"><div><?php echo $config['foot_info']; ?></div></div></div>
       </div>
+
 
     </div>
   </div>
